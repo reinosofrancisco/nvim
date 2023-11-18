05e29e6eaa0e1e9eae6fd56912961641a189e64a
reinosofrancisco <francisco.reinoso@softtek.com>
2023-11-18 15:44:26 -0300

    Telescope <Leader>gg live_grep

--- lua/after/plugins/telescope.lua
+++ lua/after/plugins/telescope.lua
@@ -1,2 +1,3 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gg', builtin.live_grep, {})
